8fa2bbea3be603a16e376a86a10e3ac694d26122
nizar <n.bahajoub@gmail.com>
2024-06-24 23:44:28 +0000

    Task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -54,17 +54,15 @@ class Server:
         return []
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, str]:
-        data = self.dataset()
-        current_data = self.get_page(page, page_size)
+        data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
 
-        total_pages = len(data) // page_size
-        next_page = page + 1 if page * page_size < len(data) else None
-        prev_page = page - 1 if page > 1 else None
         return {
-                'page_size': page_size,
+                'page_size': len(data),
                 'page': page,
-                'data': current_data,
-                'next_page': next_page,
-                'prev_next': prev_page,
+                'data': data,
+                'next_page': page + 1 if end < len(self.__dataset) else None,
+                'prev_next': page - 1 if start > 0 else None,
                 'total_pages': total_pages
                 }
