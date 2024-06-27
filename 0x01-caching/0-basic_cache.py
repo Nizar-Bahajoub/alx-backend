31686c65deed440a00f859be451d77055f0db755
nizar <n.bahajoub@gmail.com>
2024-06-27 23:51:39 +0000

    Task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,15 +5,21 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """BasiCache Documented
+    """
     def __init__(self):
         super().__init__()
 
     def put(self, key, item):
+        """Put method adding itenms inside the cach_data
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """getting items from cache_data
+        """
         if key not in self.cache_data.keys() or key is None:
             return None
         return self.cache_data[key]
